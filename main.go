4392ab9fa6e15bd9c1d6a165d7db4f4bfc93f617
vshumeyko92 <123463455+vshumeyko92@users.noreply.github.com>
2023-06-24 11:28:33 +0300

    Add files via upload

--- main.go
+++ main.go
@@ -1,33 +1,69 @@
 package main
 
 import (
+	"github.com/gin-gonic/gin"
+	"html/template"
 	"log"
-	"github.com/tarantool/go-tarantool"
-	"./task"
-	"./menu"
 	"net/http"
+	"strconv"
 )
 
-func init() {
-	http.Handle("/", http.FileServer(http.Dir("public")))
+type Task struct {
+	ID     int    `json:"id"`
+	Name   string `json:"name"`
+	Status string `json:"status"`
 }
 
+var tasks []Task
+
 func main() {
-	// Устанавливаем соединение с Tarantool
-	opts := tarantool.Opts{
-		User: "guest",
-	}
-	conn, err := tarantool.Connect("localhost:3301", opts)
-	if err != nil {
-		log.Fatal("Failed to connect to Tarantool:", err)
-	}
-	defer conn.Close()
+	router := gin.Default()
 
-	// Инициализируем модуль для работы с задачами
-	taskMod := &taskModule{
-		conn: conn,
-	}
+	// Загружаем шаблоны HTML
+	router.SetHTMLTemplate(template.Must(template.ParseFiles("templates/index.html")))
 
-	// Отображаем главное меню
-	menu.showMenu(taskMod)
+	// Маршрут для главной страницы
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", nil)
+	})
+
+	// Маршрут для получения списка задач
+	router.GET("/tasks", func(c *gin.Context) {
+		c.JSON(http.StatusOK, tasks)
+	})
+
+	// Маршрут для добавления новой задачи
+	router.POST("/tasks", func(c *gin.Context) {
+		var task Task
+		if err := c.BindJSON(&task); err != nil {
+			log.Println("Error adding task:", err)
+			c.Status(http.StatusBadRequest)
+			return
+		}
+
+		task.ID = len(tasks) + 1
+		tasks = append(tasks, task)
+
+		c.JSON(http.StatusOK, task)
+	})
+
+	// Маршрут для изменения статуса задачи
+	router.PATCH("/tasks/:id/status", func(c *gin.Context) {
+		taskID := c.Param("id")
+		newStatus := c.Query("status")
+
+		for i, task := range tasks {
+			if strconv.Itoa(task.ID) == taskID {
+				tasks[i].Status = newStatus
+				break
+			}
+		}
+
+		c.Status(http.StatusOK)
+	})
+
+	// Запуск веб-сервера
+	if err := router.Run(":8080"); err != nil {
+		log.Fatal("Failed to start server:", err)
+	}
 }
